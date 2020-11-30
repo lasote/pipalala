70b122b970a3842ba4d0455686aa71291031f1ef
Luis Martinez de Bartolome <lasote@gmail.com>
2020-11-30 13:55:40 +0100

    contents

---
+++ app/scripts/main.js
@@ -0,0 +1,21 @@
+console.log('\'Allo \'Allo!');
+
+// Uncomment to enable Bootstrap tooltips
+// https://getbootstrap.com/docs/4.0/components/tooltips/#example-enable-tooltips-everywhere
+// $(function () { $('[data-toggle="tooltip"]').tooltip(); });
+
+// Uncomment to enable Bootstrap popovers
+// https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
+// $(function () { $('[data-toggle="popover"]').popover(); });
+
+$( '#pi' ).click(function() {
+  $( '#pi' ).effect( 'bounce', { times: 3 }, 'slow' );
+});
+
+$( '#palala' ).click(function() {
+  $( '#palala' ).effect( 'bounce', { times: 3 }, 'slow' );
+});
+
+$( '#friendship' ).click(function() {
+  $( '#friendship' ).effect( 'bounce', { times: 3 }, 'slow' );
+});
